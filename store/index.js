c432cbc708c1eee8f40f9e075efc83b08167cffe
emily.lin <tiahi5914@gmail.com>
2018-12-30 11:21:17 +0800

    算是修好了（？

--- store/index.js
+++ store/index.js
@@ -1,18 +1,10 @@
 import Vuex from 'vuex'
+import _ from 'lodash'
 
 const createStore = () => {
   return new Vuex.Store({
     state: {
-      carts: [
-        {
-          id: 1,
-          name: '馬卡龍',
-          price: 450,
-          imgUrl: 'https://bit.ly/2OUteif',
-          category: '新品上市',
-          qty: 1
-        }
-      ]
+      carts: []
     },
     getters: {
       items_count_in_cart: state => {
@@ -24,18 +16,57 @@ const createStore = () => {
       },
       items: state => {
         if (state.carts !== null) {
-          const result = state.carts.filter((element, index, self) => {
-            return self.indexOf(element) === index
-          })
-          return result
+          let items = []
+          let counter = {}
+          for (let item of state.carts) {
+            if (!counter[item.id]) {
+              counter[item.id] = 0
+            }
+            // count each item
+            counter[item.id]++
+          }
+          for (let id of Object.keys(counter)) {
+            for (let cartItem of state.carts) {
+              if (cartItem.id == id) {
+                let itemToPush = Object.assign({}, cartItem)
+                itemToPush.qty = counter[id]
+                items.push(itemToPush)
+                console.log(itemToPush)
+                break
+              }
+            }
+          }
+          return items
+          // return _.uniqBy(state.carts, 'id')
+          // const result = state.carts.filter((element, index, self) => {
+          //   return self.indexOf(element) === index
+          // })
+          // return result
         } else {
           return state.carts
         }
       },
       sum: state => {
-        let r = state.carts.filter(function(element, index, self) {
-          return self.indexOf(element) === index
-        })
+        let r = []
+        let counter = {}
+        for (let item of state.carts) {
+          if (!counter[item.id]) {
+            counter[item.id] = 0
+          }
+          // count each item
+          counter[item.id]++
+        }
+        for (let id of Object.keys(counter)) {
+          for (let cartItem of state.carts) {
+            if (cartItem.id == id) {
+              let itemToPush = Object.assign({}, cartItem)
+              itemToPush.qty = counter[id]
+              r.push(itemToPush)
+              break
+            }
+          }
+        }
+
         let num = 0
         for (let i = 0; i < r.length; i++) {
           num += r[i].price * r[i].qty
@@ -52,28 +83,23 @@ const createStore = () => {
           state.carts = []
         }
         state.carts.push(product)
-        if (!product.qty) {
-          product.qty = 1
-        } else {
-          product.qty += 1
-        }
         let productsStr = JSON.stringify(state.carts)
         sessionStorage.setItem('products', productsStr)
       },
       minus_item: (state, product) => {
         let index = state.carts.indexOf(product)
+        console.log(product)
+        console.log(state.carts)
         state.carts.splice(index, 1)
-        product.qty -= 1
         let productsStr = JSON.stringify(state.carts)
         sessionStorage.setItem('products', productsStr)
       },
       delete_item: (state, product) => {
         //把carts裡面和傳進來這個product相同的item都刪掉
         let r = state.carts.filter(function(element) {
-          return element !== product //true的話就丟進去
+          return element.id !== product.id //true的話就丟進去
         })
         state.carts = r
-        product.qty = 0
         let productsStr = JSON.stringify(state.carts)
         sessionStorage.setItem('products', productsStr)
       }
